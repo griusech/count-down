47695492f5bb764b4d756f4fc18e9ec73dbd1a97
Guillermo Riusech <guillermo.riusech@educabot.com>
2021-11-08 13:03:34 -0300

    first part countDown

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import CountDown from './component/CountDown';
+
+function App() {
+  return (
+    <div className="App">
+      <CountDown />
+    </div>
+  );
+}
+
+export default App;
